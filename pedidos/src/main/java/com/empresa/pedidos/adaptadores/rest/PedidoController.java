38b73f9a10a0c76b2fff3c943e02a93a991a41f2
Ashh2222 <ashleeanyelina12@gmail.com>
2026-05-17 02:48:41 -0500

    fix: arregla error en PedidoController

--- pedidos/src/main/java/com/empresa/pedidos/adaptadores/rest/PedidoController.java
+++ pedidos/src/main/java/com/empresa/pedidos/adaptadores/rest/PedidoController.java
@@ -3,7 +3,11 @@ package com.empresa.pedidos.adaptadores.rest;
 import com.empresa.pedidos.adaptadores.facade.FachadaPedidos;
 import com.empresa.pedidos.dominio.Pedido;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/pedidos")
@@ -13,6 +17,11 @@ public class PedidoController {
 
     public PedidoController(FachadaPedidos fachada) {
         this.fachada = fachada;
+    }
+
+    @GetMapping
+    public String prueba() {
+        return "API funcionando";
     }
 
     @PostMapping
